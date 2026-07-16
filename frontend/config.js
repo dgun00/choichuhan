c7c1eab95a85bd90200194a9d59910df2b8314e2
inyoung <0828_iyiy@naver.com>
2026-07-16 09:12:28 +0900

    config.js 수정

--- frontend/config.js
+++ frontend/config.js
@@ -6,4 +6,4 @@
  *   아래 값을 실제 Render 백엔드 주소로 변경하세요.
  *   예: https://localhub-api.onrender.com
  */
-window.LOCALHUB_API_BASE = "http://127.0.0.1:8000";
+window.LOCALHUB_API_BASE = "https://choichuhan.onrender.com";
